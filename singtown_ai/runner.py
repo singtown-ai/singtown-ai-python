a3556fed1e3534493edb927a626437ef7fbc8059
zhangengru <gengru.zhan@singtown.cc>
2025-03-12 16:42:19 +0800

    refactor

--- singtown_ai/runner.py
+++ singtown_ai/runner.py
@@ -6,71 +6,53 @@ import requests
 from enum import Enum
 import csv
 import time
+import json
 import subprocess
 import tempfile
 import zipfile
 import argparse
 
-
-class Annotation(BaseModel):
-    name: str
-    label: str
-    test: bool
-    url: str
-
-
 class TaskStatus(str, Enum):
-    pending = "pending"
-    running = "running"
-    succees = "success"
-    failed = "failed"
+    pending = "PENDING"
+    running = "RUNNING"
+    success = "SUCCESS"
+    failed = "FAILED"
 
 
-class Metric(BaseModel):
-    epoch: int
-    metric: str
-
-
-class Task(BaseModel):
-    id: UUID
+class TaskResponse(BaseModel):
+    id: int
     status: TaskStatus
-    crated_at: str
-    cwd: str
-    metrics_path: str | None
-    resource_extract: str
-    output_file: str
+    created: str
+    name: str
+    epochs: int
     cmd: List[str]
 
 
-def read_csv(csv_path: str):
-    if os.path.exists(csv_path):
-        with open(csv_path, newline="") as csvfile:
-            reader = csv.DictReader(csvfile)
-            return [row for row in reader]
-    return []
-
-
 class Runner:
-    def __init__(self, host: str, task_id: UUID, token: str):
-        self.HOST = host
-        self.HEADERS = {"Authorization": f"Bearer {token}"}
+    def __init__(self, host: str, task_id: UUID, token: str, cwd: str, metrics_file, output_file, dataset_dir):
+        self.host = host
+        self.headers = {"Authorization": f"Bearer {token}"}
+        self.cwd = cwd
+        self.metrics_file = metrics_file
+        self.output_file = output_file
+        self.dataset_dir = dataset_dir
 
-        response = self.__request("GET", f"/tasks/{task_id}")
-        self.task = Task(**response.json())
-        self.__update_status(TaskStatus.running)
+        response = self.__request("GET", f"/api/v1/task/tasks/{task_id}")
+        self.task = TaskResponse(**response.json())
+        self.running()
 
     def __request(self, method: str, url: str, json: dict | None = None):
         if method == "GET":
             response = requests.get(
-                f"{self.HOST}{url}",
+                f"{self.host}{url}",
                 json=json,
-                headers=self.HEADERS,
+                headers=self.headers,
             )
         elif method == "POST":
             response = requests.post(
-                f"{self.HOST}{url}",
+                f"{self.host}{url}",
                 json=json,
-                headers=self.HEADERS,
+                headers=self.headers,
             )
         else:
             raise RuntimeError(f"request method not support {method}")
@@ -82,9 +64,9 @@ class Runner:
     def __upload(self, url: str, file_path: str):
         with open(file_path, "rb") as f:
             response = requests.post(
-                f"{self.HOST}{url}",
+                f"{self.host}{url}",
                 files={"file": f},
-                headers=self.HEADERS,
+                headers=self.headers,
             )
         if not response.ok:
             raise RuntimeError(
@@ -93,51 +75,60 @@ class Runner:
         else:
             return response
 
-    def __update_status(self, status: TaskStatus):
+    def __update_task(self, json: dict):
         self.__request(
             "POST",
-            f"/tasks/{self.task.id}",
-            {"status": status},
+            f"/api/v1/task/tasks/{self.task.id}",
+            json,
         )
 
-    def succees(self):
-        self.__update_status(TaskStatus.succees)
+    def __read_csv(self, csv_path: str) -> List[str]:
+        if os.path.exists(csv_path):
+            with open(csv_path, newline="", encoding="utf-8") as csvfile:
+                reader = csv.DictReader(csvfile)
+                return [json.dumps(row) for row in reader]  # 每行转换为 JSON 字符串
+        return []
+
+    def success(self):
+        self.__update_task({"status": TaskStatus.success})
 
     def failed(self):
-        self.__update_status(TaskStatus.failed)
+        self.__update_task({"status": TaskStatus.failed})
 
-    def metrics(self, metrics: dict):
-        self.__request("POST", f"/tasks/{self.task.id}", {"metrics": metrics})
+    def running(self):
+        self.__update_task({"status": TaskStatus.running})
+
+    def metrics(self, metrics: List[str]):
+        self.__update_task({"metrics": metrics})
 
     def log(self, log: str):
-        self.__request("POST", f"/tasks/{self.task.id}", {"log": log})
-        print("log:", log, end="", flush=True)
+        self.__update_task({"log": log})
 
-    def save(self, result_file: str):
-        self.__upload(f"/tasks/{self.task.id}/result", result_file)
+    def upload(self, result_file: str):
+        self.__upload(f"/api/v1/task/tasks/{self.task.id}/result", result_file)
 
-    def download_resource(self, extract: str):
+    def download_resource(self):
         self.log("downloading resource\n")
         filename = None
-        os.makedirs(extract, exist_ok=True)
-        response = self.__request("GET", f"/tasks/{self.task.id}/resource")
+        os.makedirs(self.dataset_dir, exist_ok=True)
+        response = self.__request("GET", f"/api/v1/task/tasks/{self.task.id}/pack")
         with tempfile.NamedTemporaryFile(delete=False) as f:
             f.write(response.content)
             filename = f.name
         with zipfile.ZipFile(filename, "r") as zip_ref:
-            zip_ref.extractall(extract)
+            zip_ref.extractall(self.dataset_dir)
         os.remove(filename)
         self.log("download success\n")
 
     def watch(
-        self, args: List[str], cwd: str, metrics_file: str | None, output_file: str
+        self, args: List[str]
     ):
         process = subprocess.Popen(
             args,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True,
-            cwd=cwd,
+            cwd=self.cwd,
         )
         stdout = ""
         last_time = time.time()
@@ -146,31 +137,39 @@ class Runner:
             now = time.time()
             if now - last_time > 3:
                 self.log(stdout)
-                if metrics_file and metrics_file.endswith(".csv"):
-                    metrics = read_csv(os.path.join(cwd, metrics_file))
+                if self.metrics_file and self.metrics_file.endswith(".csv"):
+                    metrics = self.__read_csv(os.path.join(self.cwd, self.metrics_file))
                     self.metrics(metrics)
                 stdout = ""
                 last_time = now
 
         code = process.wait()
+        
         if code == 0:
-            self.save(os.path.join(cwd, output_file))
-            self.succees()
+            self.upload(os.path.join(self.cwd, self.output_file))
+            self.success()
         else:
             for line in process.stderr:
                 print(line, end="", flush=True)
             self.failed()
 
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SingTown AI Runner")
-    parser.add_argument("--host", type=str, help="host")
-    parser.add_argument("--task", type=str, help="task id")
-    parser.add_argument("--token", type=str, help="task token")
+
+    parser.add_argument("--version", action="version", version="0.0.1")
+    parser.add_argument("--host", type=str, help="host", required=True)
+    parser.add_argument("--task", type=str, help="task id", required=True)
+    parser.add_argument("--token", type=str, help="task token", required=True)
+    parser.add_argument("--cwd", type=str, help="current working directory", default="./", required=False)
+    parser.add_argument("--metrics_file", type=str, help="metrics file", default="metrics.csv", required=False)
+    parser.add_argument("--output_file", type=str, help="output file", default="output.zip", required=False)
+    parser.add_argument("--dataset_dir", type=str, help="dataset directory", default="dataset", required=False)
     args = parser.parse_args()
 
-    run = Runner(args.host, args.task, args.token)
+    run = Runner(args.host, args.task, args.token, args.cwd, args.metrics_file, args.output_file, args.dataset_dir)
     task = run.task
 
-    run.download_resource(task.resource_extract)
-    run.watch(task.cmd, task.cwd, task.metrics_path, task.output_file)
+    run.download_resource()
+    run.watch(task.cmd)
